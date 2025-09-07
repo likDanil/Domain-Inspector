9352fdb8be08005ff8f477fb721a8661c7bf3a5a
LIK (DANIL) <61782941+likDanil@users.noreply.github.com>
2025-09-07 21:27:13 +0500

    UPD (V1.1)
    Обновлено получение доменов
    - Добавлен перехват `XHR` и `fetch` запросов 
     - Теперь в списке будут также и динамические домены (не благодарите)

--- content.js
+++ content.js
@@ -1,15 +1,32 @@
+const domains = new Set();
+
+const originalFetch = window.fetch;
+window.fetch = function(...args) {
+    try {
+        const url = new URL(args[0]);
+        domains.add(url.hostname);
+    } catch (e) {}
+    return originalFetch.apply(this, args);
+};
+
+const XHRopen = XMLHttpRequest.prototype.open;
+XMLHttpRequest.prototype.open = function(method, url) {
+    try {
+        const reqUrl = new URL(url, window.location.href);
+        domains.add(reqUrl.hostname);
+    } catch (e) {}
+    return XHRopen.apply(this, arguments);
+};
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "getDomains") {
-        const domains = [...new Set(
-            performance.getEntriesByType('resource').map(r => {
-                try {
-                    return new URL(r.name).hostname;
-                } catch (e) {
-                    return null;
-                }
-            }).filter(Boolean)
-        )].sort();
+        performance.getEntriesByType('resource').forEach(r => {
+            try {
+                const url = new URL(r.name);
+                domains.add(url.hostname);
+            } catch (e) {}
+        });
 
-        sendResponse({ domains });
+        sendResponse({ domains: Array.from(domains).sort() });
     }
 });
